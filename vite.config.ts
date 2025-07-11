35974de8d5e2ed3a85c2f308e469bbffde9d9986
akko <1421193732@qq.com>
2025-07-11 13:38:42 +0800

    package fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,41 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { visualizer } from "rollup-plugin-visualizer";
+import removeConsole from "vite-plugin-remove-console";
+import cdn from "vite-plugin-cdn-import";
+import viteCompression from "vite-plugin-compression";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  plugins: [
+    react(),
+    cdn({
+      modules: [
+        "axios",
+        "lodash",
+        {
+          name: "fabric",
+          var: "fabric",
+          path: "https://cdn.jsdelivr.net/npm/fabric@latest/dist/index.min.js",
+        },
+      ],
+    }),
+    tailwindcss(),
+    visualizer({ open: true }),
+    removeConsole(),
+    viteCompression({
+      threshold: 1024 * 20,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+
   server: {
     host: true,
     port: 3000,
@@ -21,6 +48,40 @@ export default defineConfig({
       "/upload": {
         target: "http://39.107.60.183:8001",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      experimentalLogSideEffects: true,
+      plugins: [visualizer({ open: true })],
+      output: {
+        experimentalMinChunkSize: 20 * 1024,
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash:6].js",
+        assetFileNames: "assets/[ext]/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("tailwindcss")) {
+              return "tailwindcss";
+            }
+            if (id.includes("react")) {
+              return "react";
+            }
+            if (id.includes("react-dom")) {
+              return "react-dom";
+            }
+            if (id.includes("react-router-dom")) {
+              return "react-react-router-dom";
+            }
+            if (id.includes("axios")) {
+              return "axios";
+            }
+            if (id.includes("lodash")) {
+              return "lodash";
+            }
+          }
+        },
       },
     },
   },
